93f1d2760a8275c74498e19999cce8b89a1daa8f
nshalimov <nnshalimov@gmail.com>
2015-03-24 01:14:17 +0300

    initial commit

---
+++ cmake/CXXFeatures.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 2.8)
+include(CheckCXXCompilerFlag)
+
+macro(use_cxx11)
+    check_cxx_compiler_flag(-std=c++11 COMPILER_SUPPORTS_CXX11)
+    check_cxx_compiler_flag(-std=c++0x COMPILER_SUPPORTS_CXX0X)
+
+    if(COMPILER_SUPPORTS_CXX11)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    elseif(COMPILER_SUPPORTS_CXX0X)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    else()
+        #message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+        #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    endif()
+endmacro()
